05872c73f26559396bf95363c8e6ec730c2a3c2b
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2023-10-22 02:38:57 +0300

    giriş sayfası tasarımı.

---
+++ Project/AspNetCoreIdentityApp.Web/AspNetCoreIdentityApp.Web/ViewModels/SignInViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.ViewModels
+{
+    public class SignInViewModel
+    {
+        public SignInViewModel() {     }
+        public SignInViewModel(string email, string password, bool rememberMe)
+        {
+            Email = email;
+            Password = password;
+            RememberMe = rememberMe;
+        }
+
+        [Display(Name = "Email :")]
+        [EmailAddress(ErrorMessage = "Email formatı yanlıştır.")]
+        [Required(ErrorMessage = "Email alanı boş bırakılamaz.")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "Şifre alanı boş bırakılamaz.")]
+        [Display(Name = "Şifre :")]
+        public string? Password { get; set; }
+        [Display(Name = "Beni Hatırla")]
+
+        public bool RememberMe { get; set; }
+    }
+}
