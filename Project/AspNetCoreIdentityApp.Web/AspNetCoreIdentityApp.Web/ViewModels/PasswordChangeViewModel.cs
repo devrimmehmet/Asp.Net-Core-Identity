beb7c2ef0f3adbe33d3bdbbf4de341b06419b0ea
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2023-10-28 16:18:10 +0300

    PasswordChangeViewModel in tasarlanması.

---
+++ Project/AspNetCoreIdentityApp.Web/AspNetCoreIdentityApp.Web/ViewModels/PasswordChangeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.ViewModels
+{
+    public class PasswordChangeViewModel
+    {
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Yeni Şifre alanı boş bırakılamaz.")]
+        [Display(Name = "Eski Şifre :")]
+
+        public string PasswordOld { get; set; } = null!;
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Yeni Şifre alanı boş bırakılamaz.")]
+        [Display(Name = "Yeni Şifre :")]
+        [MinLength(6, ErrorMessage = "Şifreniz en az 6 karakter olabilir")]
+        public string PasswordNew { get; set; } = null!;
+        [DataType(DataType.Password)]
+        [Compare(nameof(PasswordNew), ErrorMessage = "Şifreler aynı değildir.")]
+        [Required(ErrorMessage = "Yeni Şifre Tekrar alanı boş bırakılamaz.")]
+        [Display(Name = "Yeni Şifre Tekrar :")]
+        [MinLength(6, ErrorMessage = "Şifreniz en az 6 karakter olabilir")]
+        public string PasswordConfirm  { get; set; } = null!;
+    }
+}
