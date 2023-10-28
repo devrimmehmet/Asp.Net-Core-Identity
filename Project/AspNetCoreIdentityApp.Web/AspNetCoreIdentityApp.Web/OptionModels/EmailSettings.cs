13f161e4e58d0df165927453e09782b357683eda
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2023-10-28 17:39:39 +0300

    Null olabilir ifadelerde uyarı çözümlemesi.

--- Project/AspNetCoreIdentityApp.Web/AspNetCoreIdentityApp.Web/OptionModels/EmailSettings.cs
+++ Project/AspNetCoreIdentityApp.Web/AspNetCoreIdentityApp.Web/OptionModels/EmailSettings.cs
@@ -2,7 +2,7 @@
 {
     public class EmailSettings
     {
-        public string? Host { get; set; }
+        public string Host { get; set; } = null!; // buradaki null olamaz ifadesi ile ? ifadesi aynı anlama geliyor.
         public string? Password { get; set; }
         public string? Email { get; set; }
     }
